15830d26f3945802b61031cb988de61b91ac144b
Elle <lorijaneg@gmail.com>
2019-03-19 20:22:09 -0400

    Copyright footer

--- footer.php
+++ footer.php
@@ -21,7 +21,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sydney' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'sydney' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %2$s by %1$s.', 'sydney' ), 'aThemes', '<a href="https://athemes.com/theme/sydney" rel="designer">Sydney</a>' ); ?>
 			<div class="footer-wrapper">
@@ -92,6 +91,9 @@
 				</footer>
 				<div class="footer-mountains"></div>
 			</div> <!-- CLOSE FOOTER WRAPPER -->
+			<article class="pi-copyright">
+				<p>Created by Coral Creative &copy;<?= date('Y'); ?> </p>
+			</article>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
